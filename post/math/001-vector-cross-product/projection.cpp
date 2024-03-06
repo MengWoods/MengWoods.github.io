fce79eb0aa1a7669303b5cb81cf6afdef3dcc14a
woods <freelightingwmh@gmail.com>
2024-03-06 23:41:34 +0200

    s

--- post/math/001-vector-cross-product/projection.cpp
+++ post/math/001-vector-cross-product/projection.cpp
@@ -14,11 +14,29 @@ struct Point
     {
         return {A.x + x, A.y + y, A.z + z};
     }
+    bool operator==(Point A) const
+    {
+        if (A.x == x and A.y == y and A.z == z)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
 };
 
 double dotProduct(Point A, Point B)
 {
     return A.x * B.x + A.y * B.y + A.z * B.z;
+}
+
+Point crossProduct(Point A, Point B)
+{
+    return {A.y * B.z - A.z * B.y,
+            A.x * B.z - A.z * B.x,
+            A.x * B.y - A.y * B.x};
 }
 
 Point calcProjection(Point A, Point B, Point P)
@@ -31,15 +49,42 @@ Point calcProjection(Point A, Point B, Point P)
     return C;
 }
 
+bool verifyProjection(Point A, Point B, Point P, Point C)
+{
+    Point AC = {C.x - A.x, C.y - A.y, C.z - A.z};
+    Point AB = {B.x - A.x, B.y - A.y, B.z - A.z};
+    Point PC = {C.x - P.x, C.y - P.y, C.z - P.z};
+    double dot_product = dotProduct(PC, AB);
+    Point cross_product = crossProduct(AC, AB);
+    Point zero_vec = {0, 0, 0};
+    if (dot_product == 0 and cross_product == zero_vec)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
 int main()
 {
     Point A = {1, 2, 3};
     Point B = {4, 5, 6};
     Point P = {2, 3, 4};
     
-    Point projectionPoint = calcProjection(A, B, P);
+    Point C = calcProjection(A, B, P);
 
-    cout << "Projection Point C: (" << projectionPoint.x << ", " << projectionPoint.y << ", " << projectionPoint.z << ")" << endl;
+    cout << "Projection Point C: (" << C.x << ", " << C.y << ", " << C.z << ")" << endl;
+
+    if (verifyProjection(A, B, P, C))
+    {
+        cout << "Correct!" << endl;
+    }
+    else
+    {
+        cout << "Incorrect." << endl;
+    }
 
     return 0;
 }
